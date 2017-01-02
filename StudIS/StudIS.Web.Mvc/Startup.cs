b9a7f784746efbecff70eac1024665c1f0e50252
zeljko-b <zeljko.baranek@fer.hr>
2017-01-02 17:25:13 +0100

    Solution with default project templates for Models, DAL, Services, Web.Mvc, Web.Api and Desktop

---
+++ StudIS/StudIS.Web.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudIS.Web.Mvc.Startup))]
+namespace StudIS.Web.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
