0a131547fa4e23066fe766aceb63d8a6f2fd947c
zlatkoHrastic <zlatko.hr8@gmail.com>
2017-01-25 21:35:01 +0100

    Personal data for students, layout for students and lecturers has to be separated

--- StudIS/StudIS.Web.Mvc/Controllers/StudentController.cs
+++ StudIS/StudIS.Web.Mvc/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using StudIS.Models.RepositoryInterfaces;
+using StudIS.Models.Users;
 using StudIS.Services;
 using StudIS.Web.Mvc.Models;
 using System;
@@ -62,6 +63,9 @@ namespace StudIS.Web.Mvc.Controllers
         /// <returns></returns>
         public ActionResult ScoreInfo(int id)
         {
+            if (Session["userId"] == null)
+                return RedirectToAction("Index", "Home");
+
             ViewBag.Email = Session["email"];
             ViewBag.Title = "Bodovi";
             var studentId = (int)Session["userId"];
@@ -87,6 +91,26 @@ namespace StudIS.Web.Mvc.Controllers
 
             };
             return View(scoredCourse);
+        }
+
+        public ActionResult PersonalData()
+        {
+            if (Session["userId"] == null)
+                return RedirectToAction("Index", "Home");
+
+            ViewBag.Email = Session["email"];
+            ViewBag.Title = "Osobni podaci";
+            var studentId = (int)Session["userId"];
+            var user = _userRepository.GetById(studentId);
+
+            if(user==null || !UserServices.isUserStudent(user))
+                return RedirectToAction("Index", "Home");
+
+            return View(new StudentViewModel((Student)user));
+
+
+          
+
         }
 
     }
---
+++ StudIS/StudIS.Web.Mvc/Models/StudentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using StudIS.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudIS.Web.Mvc.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string NationalIdentificationNumber { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string StudentIdentificationNumber { get; set; }
+
+        public StudentViewModel()
+        {
+
+        }
+        public StudentViewModel(Student student)
+        {
+            Id = student.Id;
+            Name = student.Name;
+            Surname = student.Surname;
+            NationalIdentificationNumber = student.NationalIdentificationNumber;
+            Email = student.Email;
+            FullName = student.FullName;
+            StudentIdentificationNumber = student.StudentIdentificationNumber;
+        }
+    }
+}
