3a8d4f023b0a7eb7a97227b07d1260b588369eb1
zlatkoHrastic <zlatko.hr8@gmail.com>
2017-01-26 22:35:25 +0100

    Course services for creating,updateing and deleteing courses

--- StudIS/StudIS.Services/CourseServices.cs
+++ StudIS/StudIS.Services/CourseServices.cs
@@ -1,5 +1,6 @@
 ﻿using StudIS.Models;
 using StudIS.Models.RepositoryInterfaces;
+using StudIS.Models.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,15 @@ namespace StudIS.Services
     {
 
         private ICourseRepository _courseRepository;
+        private IUserRepository _userRepository;
+        private IComponentRepository _componentRepository;
 
-        public CourseServices(ICourseRepository courseRepository)
+        public CourseServices(ICourseRepository courseRepository, IUserRepository userRepository, IComponentRepository componentRepository)
         {
 
             _courseRepository = courseRepository;
+            _userRepository = userRepository;
+            _componentRepository = componentRepository;
 
         }
         public List<Course> GetCoursesByUserId(int id)
@@ -31,8 +36,108 @@ namespace StudIS.Services
 
         }
 
-        public Course GetCourseById(int courseId) {
+        public Course GetCourseById(int courseId)
+        {
             return _courseRepository.GetById(courseId);
         }
+
+        public Course CreateCourse(string name, string naturalIdentifier, int ectsCredits)
+        {
+            var course = new Course()
+            {
+                Name = name,
+                NaturalIdentifier = naturalIdentifier,
+                EctsCredits = ectsCredits
+            };
+            return _courseRepository.Create(course);
+
+        }
+
+
+        public Course UpdateCourse(int courseId, string name, string naturalIdentifier, int ectsCredits)
+        {
+            var course = _courseRepository.GetById(courseId);
+            if (course == null)
+                return null;
+
+            course.Name = name;
+            course.NaturalIdentifier = naturalIdentifier;
+            course.EctsCredits = ectsCredits;
+            return _courseRepository.Create(course);
+
+        }
+
+        public bool DeleteCourse(int id)
+        {
+            return _componentRepository.DeleteById(id);
+        }
+        public Course AddLecturer(int courseId, int lecturerId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var lecturer = _userRepository.GetById(lecturerId);
+
+            if (course == null || lecturer == null || !UserServices.isUserLecturer(lecturer))
+                return null;
+            course.LecturersInCharge.Add((Lecturer)lecturer);
+            return _courseRepository.Update(course);
+        }
+
+        public Course RemoveLecturer(int courseId, int lecturerId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var lecturer = _userRepository.GetById(lecturerId);
+
+            if (course == null || lecturer == null || !UserServices.isUserLecturer(lecturer))
+                return null;
+            course.LecturersInCharge.Remove((Lecturer)lecturer);
+            return _courseRepository.Update(course);
+        }
+
+
+        public Course AddStudent(int courseId, int studentId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var student = _userRepository.GetById(studentId);
+
+            if (course == null || student == null || !UserServices.isUserStudent(student))
+                return null;
+            course.StudentsEnrolled.Add((Student)student);
+            return _courseRepository.Update(course);
+        }
+
+        public Course RemoveStudent(int courseId, int studentId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var student = _userRepository.GetById(studentId);
+
+            if (course == null || student == null || !UserServices.isUserStudent(student))
+                return null;
+            course.StudentsEnrolled.Remove((Student)student);
+            return _courseRepository.Update(course);
+        }
+
+
+        public Course AddComponent(int courseId, int componentId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var component = _componentRepository.GetById(courseId);
+
+            if (course == null || component == null)
+                return null;
+            course.Components.Add(component);
+            return _courseRepository.Update(course);
+        }
+
+        public Course RemoveComponent(int courseId, int componentId)
+        {
+            var course = _courseRepository.GetById(courseId);
+            var component = _componentRepository.GetById(courseId);
+
+            if (course == null || component == null)
+                return null;
+            course.Components.Remove(component);
+            return _courseRepository.Update(course);
+        }
+
     }
 }
