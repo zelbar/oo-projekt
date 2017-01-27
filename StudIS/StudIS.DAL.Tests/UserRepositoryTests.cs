204924437e1d99dada2231652e646cf6bda81945
zlatkoHrastic <zlatko.hr8@gmail.com>
2017-01-27 11:56:10 +0100

    User repository tests

--- StudIS/StudIS.DAL.Tests/UserRepositoryTests.cs
+++ StudIS/StudIS.DAL.Tests/UserRepositoryTests.cs
@@ -20,15 +20,15 @@ namespace StudIS.DAL.Tests
         [TestMethod]
         public void CreateTest_Success()
         {
-            var student = new Administrator()
+            var admin = new Administrator()
             {
-                Email="admin@admin.ad",
-                Name="Adminko",
-                Surname="Adminic",
-                NationalIdentificationNumber="12345",
-                PasswordHash="xxx"
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
             };
-            var created=_usrRep.Create(student);
+            var created = _usrRep.Create(admin);
 
             Assert.IsNotNull(created);
             Assert.IsFalse(created.Id == 0);
@@ -37,7 +37,7 @@ namespace StudIS.DAL.Tests
         [TestMethod]
         public void CreateTest_Fail()
         {
-            var student = new Administrator()
+            var admin = new Administrator()
             {
                 Email = "admin@admin.ad",
                 Name = "Adminko",
@@ -45,9 +45,170 @@ namespace StudIS.DAL.Tests
                 NationalIdentificationNumber = "12345",
                 PasswordHash = null
             };
-            var created = _usrRep.Create(student);
+            try
+            {
+                var created = _usrRep.Create(admin);
+                Assert.Fail("No exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex is NHibernate.PropertyValueException);
+            }
 
-            Assert.IsNull(created);
+
+        }
+
+
+        [TestMethod]
+        public void DeleteTest_True()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+
+            var wasDeleted = _usrRep.DeleteById(created.Id);
+
+
+            Assert.IsTrue(wasDeleted);
+        }
+
+
+        [TestMethod]
+        public void DeleteTest_False()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+
+            _usrRep.DeleteById(created.Id);
+            var wasDeleted = _usrRep.DeleteById(created.Id);
+
+
+            Assert.IsFalse(wasDeleted);
+        }
+
+        [TestMethod]
+        public void GetByIdTest_Correct()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+
+            var user = _usrRep.GetById(created.Id);
+
+
+            Assert.IsNotNull(user);
+
+        }
+
+
+        [TestMethod]
+        public void GetByIdTest_InCorrect()
+        {
+            _usrRep.DeleteById(10);
+            var user = _usrRep.GetById(10);
+            Assert.IsNull(user);
+
+        }
+
+        [TestMethod]
+        public void GetByEmailTest_Correct()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+            var user = _usrRep.GetByEmail(created.Email);
+            Assert.IsNotNull(user);
+
+        }
+
+        [TestMethod]
+        public void GetByEmailTest_InCorrect()
+        {
+
+            var user = _usrRep.GetByEmail("wrongemail");
+            Assert.IsNull(user);
+
+        }
+
+
+        [TestMethod]
+        public void UpdateUser_Success()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+
+            created.Name = "UpdatedAdminko";
+            var updated = _usrRep.Update(created);
+
+            Assert.IsNotNull(updated);
+            Assert.AreEqual(updated.Name, "UpdatedAdminko");
+
+        }
+
+
+
+        [TestMethod]
+        public void UpdateUser_Fail()
+        {
+            var admin = new Administrator()
+            {
+                Email = "admin@admin.ad",
+                Name = "Adminko",
+                Surname = "Adminic",
+                NationalIdentificationNumber = "12345",
+                PasswordHash = "xxx"
+            };
+
+            var created = _usrRep.Create(admin);
+
+            created.PasswordHash = null;
+
+            try
+            {
+                var updated = _usrRep.Update(created);
+                Assert.Fail("No exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex is NHibernate.PropertyValueException);
+            }
         }
 
     }
