0d0cd2c0f9fc9eebf59e582e15cd0796101d84f5
zeljko-b <zeljko.baranek@fer.hr>
2017-01-27 22:57:44 +0100

    UserForm DI for services fix in StudIS.Desktop.Tests

--- StudIS/StudIS.Desktop.Tests/UserFormTests.cs
+++ StudIS/StudIS.Desktop.Tests/UserFormTests.cs
@@ -17,8 +17,11 @@ namespace StudIS.Desktop.Tests
         private readonly IUserRepository _userRepository;
         private readonly ICourseRepository _courseRepository;
         private readonly IComponentRepository _componentRepository;
+        private readonly IScoreRepository _scoreRepository;
+
         private readonly UserServices _userServices;
         private readonly CourseServices _courseServices;
+        private readonly ScoreServices _scoreServices;
 
         private readonly Student student = new Student()
         {
@@ -37,14 +40,17 @@ namespace StudIS.Desktop.Tests
             _userRepository = new UserRepository(nhs);
             _courseRepository = new CourseRepository(nhs);
             _componentRepository = new ComponentRepository(nhs);
+            _scoreRepository = new ScoreRepository(nhs);
+
             _userServices = new UserServices(_userRepository);
             _courseServices = new CourseServices(_courseRepository, _userRepository, _componentRepository);
+            _scoreServices = new ScoreServices(_scoreRepository, _courseRepository, _userRepository);
         }
 
         [TestMethod]
         public void UserForm_CreateUser()
         {
-            var controller = new UserFormController(_userRepository, _courseRepository);
+            var controller = new UserFormController(_userServices, _courseServices);
             var success = controller.SaveUser(student);
 
             Assert.IsTrue(success);
@@ -53,7 +59,7 @@ namespace StudIS.Desktop.Tests
         [TestMethod]
         public void UserForm_TryDeleteNonExistingUser()
         {
-            var controller = new UserFormController(_userRepository, _courseRepository);
+            var controller = new UserFormController(_userServices, _courseServices);
             var success = controller.DeleteUser(student.Email);
 
             Assert.IsFalse(success);
@@ -64,7 +70,7 @@ namespace StudIS.Desktop.Tests
         {
             this.UserForm_CreateUser();
 
-            var controller = new UserFormController(_userRepository, _courseRepository);
+            var controller = new UserFormController(_userServices, _courseServices);
             var deletionSuccess = controller.DeleteUser("eg@example.org");
             
             Assert.IsTrue(deletionSuccess);
@@ -75,7 +81,7 @@ namespace StudIS.Desktop.Tests
         {
             this.UserForm_CreateUser();
 
-            var controller = new UserFormController(_userRepository, _courseRepository);
+            var controller = new UserFormController(_userServices, _courseServices);
 
             var newName = "Pero";
             var newSurname = "Djetlic";
