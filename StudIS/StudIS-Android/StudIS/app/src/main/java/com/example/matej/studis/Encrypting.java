519ccdc5b97c700dab965889370cbb5e71a1100c
Matej <matejperos@hotmail.com>
2017-02-02 03:39:42 +0100

    Final fixes

---
+++ StudIS/StudIS-Android/StudIS/app/src/main/java/com/example/matej/studis/Encrypting.java
@@ -0,0 +1,63 @@
+package com.example.matej.studis;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Matej on 2.2.2017..
+ */
+
+public class Encrypting {
+
+    public String Encryptingpass(String text){
+
+        return SHA1(text);
+    }
+
+    public String SHA1(String text) {
+
+        try {
+
+            MessageDigest md;
+            md = MessageDigest.getInstance("SHA-1");
+            md.update(text.getBytes("UTF-8"),
+                    0, text.length());
+            byte[] sha1hash = md.digest();
+
+            return toHex(sha1hash);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String toHex(byte[] buf) {
+
+        if (buf == null) return "";
+
+        int l = buf.length;
+        StringBuffer result = new StringBuffer(2 * l);
+
+        for (int i = 0; i < buf.length; i++) {
+            appendHex(result, buf[i]);
+        }
+
+        return result.toString();
+
+    }
+
+    private final static String HEX = "0123456789ABCDEF";
+
+    private static void appendHex(StringBuffer sb, byte b) {
+
+        sb.append(HEX.charAt((b >> 4) & 0x0f))
+                .append(HEX.charAt(b & 0x0f));
+
+    }
+
+}
