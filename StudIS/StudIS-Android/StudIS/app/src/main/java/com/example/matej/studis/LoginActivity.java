f5cf623777579e679a1839934978b13906f5f2b8
Matej <matejperos@hotmail.com>
2017-02-02 03:35:36 +0100

    Final fixes

--- StudIS/StudIS-Android/StudIS/app/src/main/java/com/example/matej/studis/LoginActivity.java
+++ StudIS/StudIS-Android/StudIS/app/src/main/java/com/example/matej/studis/LoginActivity.java
@@ -52,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
-                                                       
+
 
                             if(jsonResponse.length()>0){
                                 String name = jsonResponse.getString("FullName");
@@ -84,12 +84,10 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 };
 
+                Encrypting enc = new Encrypting();
+                String encpass =  enc.Encryptingpass(pasword);
 
-                String encryptedpassword = SHA1(pasword);
-
-
-
-                LoginRequest loginRequest = new LoginRequest(email, encryptedpassword, responseListener);
+                LoginRequest loginRequest = new LoginRequest(email, encpass, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 queue.add(loginRequest);
                 //etJMBAG.setText("Kraj sam ovdje");
@@ -100,48 +98,5 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
-    public String SHA1(String text) {
 
-        try {
-
-            MessageDigest md;
-            md = MessageDigest.getInstance("SHA-1");
-            md.update(text.getBytes("UTF-8"),
-                    0, text.length());
-            byte[] sha1hash = md.digest();
-
-            return toHex(sha1hash);
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public static String toHex(byte[] buf) {
-
-        if (buf == null) return "";
-
-        int l = buf.length;
-        StringBuffer result = new StringBuffer(2 * l);
-
-        for (int i = 0; i < buf.length; i++) {
-            appendHex(result, buf[i]);
-        }
-
-        return result.toString();
-
-    }
-
-    private final static String HEX = "0123456789ABCDEF";
-
-    private static void appendHex(StringBuffer sb, byte b) {
-
-        sb.append(HEX.charAt((b >> 4) & 0x0f))
-                .append(HEX.charAt(b & 0x0f));
-
-    }
 }
